4a3ea4adb0e6e4f5f7d39eaef6a15f9a919e3170
ShotNer <72764820+ShotNer@users.noreply.github.com>
2020-10-12 15:26:15 -0500

    Add files via upload

---
+++ entornopolis-aceprtar o no/__resource.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author '69Terminator69'
+
+
+client_scripts {
+    'config.lua',
+	'client/police.lua',
+}
+
+
+server_scripts {
+    'config.lua',
+    'server/police.lua',
+}
+
+dependency 'es_extended'
